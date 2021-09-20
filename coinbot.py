4db72962fa828cddc7c8c152ebb63e41a8fddddc
Austen Haymond <ahaymon17@gmail.com>
2021-09-20 14:47:26 -0700

    added coinlayer api call for ada price

--- coinbot.py
+++ coinbot.py
@@ -1,11 +1,13 @@
 import discord
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 client = discord.Client()
-
+api_url = 'http://api.coinlayer.com/api/live'
+api_key = os.getenv('COINLAYER_KEY')
 token = os.getenv('BOT_TOKEN')
 
 @client.event
@@ -19,5 +21,13 @@ async def on_message(message):
 
     if message.content.startswith('$test'):
         await message.channel.send('this bot is working!')
+
+    if message.content.startswith('$ada'):
+        url = f'{api_url}&symbols=ADA?access_key={api_key}'
+        raw = requests.get(url).json()
+        response = []
+        response.append(raw['rates'])
+        price = response[0]
+        await message.channel.send(f'{price}')
 
 client.run(token)
